03f84b9322f492272ef825ecd966c4640ef715d8
vechro <stenmr@gmail.com>
2021-09-09 13:33:55 +0300

    Formatting

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,7 +3,11 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("replace units", |b| {
-        b.iter(|| ConversionTable::replace_units(black_box("56 gb / 6 + 4kib * 5 -4 mb + 4 B".to_string())))
+        b.iter(|| {
+            ConversionTable::replace_units(black_box(
+                "56 gb / 6 + 4kib * 5 -4 mb + 4 B".to_string(),
+            ))
+        })
     });
 }
 
